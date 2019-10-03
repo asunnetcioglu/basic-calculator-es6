a692deef5a3b269fe3bf542fc38023a002ef6e44
asunnetcioglu <asunnetcioglu@gmail.com>
2019-10-03 14:26:10 +0300

    Project end
    end of project

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,33 @@ const btnSum = document.getElementById('btn-sum');
 const btnExt = document.getElementById('btn-ext');
 const btnImp = document.getElementById('btn-imp');
 const btnDivi = document.getElementById('btn-divi');
-const sonuc = document.getElementById('result')
+const results = document.getElementById('result')
 
 // functions
 
 btnSum.addEventListener('click', () => {
-   const x = firstNumber.value;
+   //const x = firstNumber.value || 0;
+   //const y = secondNumber.value || 0;
 
- })
+   const x = parseFloat(firstNumber.value) || 0;
+   const y = parseFloat(secondNumber.value) || 0;
+   result.innerHTML = (x + y);
+ });
+
+ btnExt.addEventListener('click', () => {
+    const x = parseFloat(firstNumber.value) || 0;
+    const y = parseFloat(secondNumber.value) || 0;
+    result.innerHTML = (x - y);
+  });
+
+  btnImp.addEventListener('click', () => {
+     const x = parseFloat(firstNumber.value) || 0;
+     const y = parseFloat(secondNumber.value) || 0;
+     result.innerHTML = (x * y);
+   });
+
+   btnDivi.addEventListener('click', () => {
+      const x = parseFloat(firstNumber.value) || 0;
+      const y = parseFloat(secondNumber.value) || 0;
+      result.innerHTML = (x / y);
+    })
