abc21a9340d950178368ba1876a18129b90afe53
asunnetcioglu <asunnetcioglu@gmail.com>
2019-10-03 13:06:37 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// constants
+
+const firstNumber = document.getElementById('first-number');
+const secondNumber = document.getElementById('second-number');
+const btnSum = document.getElementById('btn-sum');
+const btnExt = document.getElementById('btn-ext');
+const btnImp = document.getElementById('btn-imp');
+const btnDivi = document.getElementById('btn-divi');
+const sonuc = document.getElementById('result')
+
+// functions
+
+btnSum.addEventListener('click', () => {
+   const x = firstNumber.value;
+
+ })
